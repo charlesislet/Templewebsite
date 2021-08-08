97f1c2e1a83bf699c06b7dc36950766ee9f5d736
Charles <charlesislet29@gmail.com>
2021-08-08 22:22:00 +0800

    update timeline

--- src/components/Historycard.js
+++ src/components/Historycard.js
@@ -1,3 +1,4 @@
+import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -7,21 +8,30 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+const useStyles = makeStyles({
+    root: {
+        width:300,
+        maxWidth:300
+    },
+});
+
 const Historycard = ( { historycard }) => {
+    const classes = useStyles();
     return (
-        <Grid item xs={12} md={3}  key={historycard.id}>
-            <Card>
+        <Grid container justifyContent="center" alignItems="center" item xs={12} md={12} key={historycard.id}>
+            <Card className={classes.root}>
                 <CardActionArea>
                     <CardMedia className="img__musician"
                     image={historycard.image}
                     title={historycard.name}
+                    height="200"
                     />
                     <CardContent>
                     <Typography gutterBottom variant="h5" component="h2">
                         {historycard.name}
                     </Typography>
                     <Typography variant="body2" color="textSecondary" component="p">
-                        {historycard.description.substring(0, 200)} ...
+                        {historycard.description} 
                     </Typography>
                     </CardContent>
                 </CardActionArea>
--- src/pages/天后History.js
+++ src/pages/天后History.js
@@ -17,57 +17,48 @@ import pic4 from '../Image/天后宮/IMG_9492.JPG';
 const allHistorycards = [
   {
     'id': 1,
-    'name': 'Ludwig van Beethoven',
-    'birth': 1770,
-    'death': 1827,
-    'description': 'Surmontant à force de volonté les épreuves d’une vie marquée par la surdité qui le frappe à vingt-sept ans, célébrant dans sa musique le triomphe de l’héroïsme et de la joie quand le destin lui prescrivait l’isolement et la misère, il sera récompensé post mortem par cette affirmation de Romain Rolland : « Il est bien davantage que le premier des musiciens. Il est la force la plus héroïque de l’art moderne ».',
-    'link': 'https://fr.wikipedia.org/wiki/Ludwig_van_Beethoven',
-    'image': pic1
+    'name': '明萬曆卅二年',
+    'year': 1604,
+    'description': '沈有容諭退紅毛番',
+    'link': '',
+    // 'image': 
   },
   {
     'id': 2,
-    'name': 'Wolfgang Amadeus Mozart',
-    'birth': 1756,
-    'death': 1791,
-    'description': 'Mort à trente-cinq ans, il laisse une œuvre impressionnante (893 œuvres sont répertoriées dans le catalogue Köchel), qui embrasse tous les genres musicaux de son époque. Selon le témoignage de ses contemporains, il était, au piano comme au violon, un virtuose.',
-    'link': 'https://fr.wikipedia.org/wiki/Wolfgang_Amadeus_Mozart',
-    'image': pic2
+    'name': '康熙廿二年',
+    'year': 1683,
+    'description': '施琅攻佔澎湖，將勝利歸功於媽祖庇佑',
+    'link': '',
+    // 'image': 
   },
   {
     'id': 3,
-    'name': 'Jean-Sébastien Bach',
-    'birth': 1685,
-    'death': 1750,
-    'description': 'Ses contemporains l’ont souvent considéré comme un musicien austère, trop savant et moins tourné vers l’avenir que certains de ses collègues. Il a formé de nombreux élèves et transmis son savoir à plusieurs fils musiciens pour lesquels il a composé quantité de pièces à vocation didactique, ne laissant cependant aucun écrit ou traité.',
-    'link': 'https://fr.wikipedia.org/wiki/Jean-S%C3%A9bastien_Bach',
-    'image': pic3
-  }
-  ,
-  {
-    'id': 4,
-    'name': 'Frédéric Chopin',
-    'birth': 1810,
-    'death': 1849,
-    'description': 'Reconnu comme l’un des plus grands compositeurs de musique de la période romantique, Frédéric Chopin est aussi l’un des plus célèbres pianistes du xixe siècle. Sa musique est encore aujourd’hui l’une des plus jouées et demeure un passage indispensable à la compréhension du répertoire pianistique universel.',
-    'link': 'https://fr.wikipedia.org/wiki/Fr%C3%A9d%C3%A9ric_Chopin',
-    'image': pic4
+    'name': '光緒十一年',
+    'year': 1885,
+    'description': '重建',
+    'link': '',
+    // 'image': 
   }
 ];
 
 const marks = [
   {
-    value: 1678,
-    label: '1678'
+    value: 1604,
+    label: '1604'
   },
   {
-    value: 1700,
-    label: '1700'
+    value: 1683,
+    label: '1683'
+  },
+  {
+    value: 1885,
+    label: '1885'
   },
 ];
 
 const PrettoSlider = withStyles({
   root: {
-    color: '#5b166d',
+    color: '#008040',
     height: 8
   },
   thumb: {
@@ -77,34 +68,46 @@ const PrettoSlider = withStyles({
     border: '2px solid currentColor',
     marginTop: -8,
     marginLeft: -12,
+    zIndex:3,
     '&:focus, &:hover, &$active': {
       boxShadow: 'inherit',
     },
   },
   active: {},
   valueLabel: {
-    left: 'calc(-50% + 4px)',
+    left: 'calc(-50% + 12px)',
   },
   track: {
-    height: 8,
-    borderRadius: 4,
+    height: 15,
+    borderRadius: 2,
   },
   rail: {
-    height: 8,
-    borderRadius: 4,
+    height: 15,
+    borderRadius: 2,
   },
+  mark: {
+    height: 15,
+    width: 5,
+    borderRadius: 6,
+    backgroundColor: '#e60000',
+    zIndex:1,
+  },
+  markActive: {
+    backgroundColor: '#00cc66',
+    zIndex:2,
+  }
 })(Slider);
 
 export default function TienHistory() {
   const [selectedHistorycards, setSelectedHistorycards] = useState([]);
   const handleSliderChange = (event, value) => {
     setSelectedHistorycards(allHistorycards.filter(element => {
-      return value >= element.birth && value <= element.death
+      return value == element.year
     }))
   }
 
   useEffect(() => {
-    handleSliderChange('', 1770);
+    handleSliderChange('', 1604);
   }, [])
   return (
     <>
@@ -115,8 +118,8 @@ export default function TienHistory() {
     </div>
     <div className='container'>
         <SimpleBreadcrumbs/>
-        <PrettoSlider className="slider" valueLabelDisplay="auto" aria-label="pretto slider" defaultValue={1770} marks={marks} min={1678} max={1897} onChange={handleSliderChange} onChangeCommitted={handleSliderChange} />
-      <Historycards selectedHistorycards={selectedHistorycards} />
+        <PrettoSlider className="slider" valueLabelDisplay="auto" aria-label="pretto slider" defaultValue={1604} marks={marks} min={1604} max={1885} onChange={handleSliderChange} onChangeCommitted={handleSliderChange} />
+        <Historycards selectedHistorycards={selectedHistorycards} />
     </div>
     </>
   );
